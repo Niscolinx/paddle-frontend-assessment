934865ad7949b5a008a041b3e51835ce15c3f563
Igboanugwo Collins <munisco12@gmail.com>
2022-07-18 15:42:59 +0100

    working on about us page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import React, { MouseEventHandler } from 'react'
-// import { ComingSoonContext } from '../pages/comingsoon'
 
 function Header({
     toShow
@@ -10,12 +9,20 @@ function Header({
     return (
         <header className='header'>
             <div className='header__logo'>
+                <Link href='/'>
+                
                 <img src='logo-white.png' alt='' />
+                </Link>
             </div>
 
             <ul className='header__lists'>
-                <li className='header__lists--item'>Abous Us</li>
-                <li className='header__lists--item'> <Link href='/blog'> Blog</Link></li>
+                <li className='header__lists--item'>
+                    <Link href='/about'> About Us</Link>
+                </li>
+                <li className='header__lists--item'>
+                    {' '}
+                    <Link href='/blog'> Blog</Link>
+                </li>
                 <li
                     className='header__lists--item header__lists--item-active'
                     onClick={toShow}
