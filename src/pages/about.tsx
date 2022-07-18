42c1fa82ffbded17ce5a921835f75305b601418c
Igboanugwo Collins <munisco12@gmail.com>
2022-07-18 18:59:24 +0100

    finished hero

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,11 +7,32 @@ const about = () => {
                 <div className='about__header--1'></div>
                 <div className='about__header--1-box'></div>
                 <div className='about__header--2'>
-                  <p><span>
-                    Built for SaaS,</span> <span>and E-commerce</span></p>
+                    <p className='about__header--2__subTitle'>
+                        <span className='span-1'></span>
+                        <span className='span-2'>ABOUT US</span>
+                    </p>
+                    <p className='about__header--2__title'>
+                        <span>Built for SaaS</span> <span>and E-commerce</span>
+                    </p>
                 </div>
-                <div className='about__header--3'>lorem</div>
+                <div className='about__header--3'>
+                    Our tools are easy to set up and use with a user friendly
+                    dashboard that enables you to set up, launch, automate and
+                    manage multi-affiliate campaigns in 5 minutes.
+                </div>
+                <div className='about__header--ellipse'></div>
             </header>
+
+            <section className='about__section1'>
+                <p className='about__section1--content'>
+                        Metricks was developed because just like you, we needed
+                        a product that could give us <span>good value</span>
+                </p>
+            </section>
+
+            <section className='about__section2'>
+          
+            </section>
         </div>
     )
 }
