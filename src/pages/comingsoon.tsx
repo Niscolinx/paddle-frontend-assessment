70aba670f97cff48eb8238eae253dd733f24f32d
Igboanugwo Collins <munisco12@gmail.com>
2022-07-18 07:00:46 +0100

    working on contact us component

--- src/pages/comingsoon.tsx
+++ src/pages/comingsoon.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-import ContactUs from '../Containers/ContactUs'
+import ContactUs from '../components/ContactUs'
 
 function comingSoon() {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -17,7 +17,60 @@ function comingSoon() {
                 <div className='comingSoon__ellipse--3'></div>
             </div>
 
-           
+            <div className='comingSoon__rapper'>
+                <h1 className='comingSoon__header'>
+                    Something awesome is coming soon
+                </h1>
+                <p className='comingSoon__text'>
+                    Your all-in-one affiliate marketing tracking software{' '}
+                    <span>track</span>, <span>automate</span> and{' '}
+                    <span>optimize</span> your campaigns.
+                </p>
+                <div className='comingSoon__timer'>
+                    <div className='comingSoon__timer--item'>
+                        <span className='span-1'>7</span>
+                        <span className='span-2'>Days</span>
+                    </div>
+                    <div className='comingSoon__timer--item'>
+                        <span className='span-1'>24</span>
+                        <span className='span-2'>Hours</span>
+                    </div>
+                    <div className='comingSoon__timer--item'>
+                        <span className='span-1'>54</span>
+                        <span className='span-2'>Minutes</span>
+                    </div>
+                    <div className='comingSoon__timer--item'>
+                        <span className='span-1'>11</span>
+                        <span className='span-2'>Second</span>
+                    </div>
+                </div>
+                <form className='comingSoon__form' onSubmit={handleSubmit}>
+                    <div className='comingSoon__form--1'>
+                        <input
+                            type='text'
+                            placeholder='First Name..'
+                            className='comingSoon__form--input'
+                        />
+                        <input
+                            type='text'
+                            placeholder='Last Name..'
+                            className='comingSoon__form--input'
+                        />
+                    </div>
+                    <div className='comingSoon__form--2'>
+                        <input
+                            type='text'
+                            placeholder='Enter your email address...'
+                            className='comingSoon__form--input-email'
+                        />
+                        <button type='submit' className='comingSoon__form--btn'>Join Our waiting List</button>
+                    </div>
+                </form>
+            </div>
+
+            <div className="comingSoon__contactUs">
+               <ContactUs/>
+            </div>
         </div>
         <Footer/>
         </>
