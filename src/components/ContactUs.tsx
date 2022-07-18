eba62cd13f87408a1f4730f94682674c934c3fe9
Igboanugwo Collins <munisco12@gmail.com>
2022-07-18 07:46:18 +0100

    working on contact us component

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -6,8 +6,14 @@ function ContactUs() {
             <div className='contactUs__left'></div>
             <div className='contactUs__right'>
                 <div className='contactUs__ellipse'></div>
-                <form className="contactUs__form">
-                  
+                <form className='contactUs__form'>
+                    <h1>
+                        Hey, we are still in the works, but you can send us a
+                        message!
+                    </h1>
+
+                    <label htmlFor='first_name'>First Name</label>
+                    <input type='text' id='first_name' name='first_name' />
                 </form>
             </div>
         </div>
